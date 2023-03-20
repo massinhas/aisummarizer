418a9b4eb058a9754d4b372be5fc367fd679fdc8
Luis Correia <luismassinhas@gmail.com>
2023-03-20 01:10:22 +0000

    update voice thing

--- static/audioHandling.js
+++ static/audioHandling.js
@@ -1,50 +1,101 @@
+// Get references to the HTML elements
 const startRecording = document.getElementById('startRecording');
 const stopRecording = document.getElementById('stopRecording');
-const audioInput = document.getElementById('audioInput');
-
+const playButton = document.getElementById('playButton');
 let mediaRecorder;
 let recordedChunks = [];
 let audioContext;
 
-async function start() {
-    try {
-        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-        mediaRecorder = new MediaRecorder(stream);
-        mediaRecorder.addEventListener('dataavailable', (e) => {
-            recordedChunks.push(e.data);
-        });
-        mediaRecorder.start();
-        stopRecording.disabled = false;
-    } catch (err) {
-        console.error(err);
-        alert('Failed to get access to microphone');
-    }
-}
+// Add an event listener to the start recording button
+startRecording.addEventListener('click', async function() {
+  // Create an AudioContext if one doesn't already exist, or resume the existing one if it's suspended
+  if (!audioContext) {
+    audioContext = new AudioContext();
+  } else if (audioContext.state === 'suspended') {
+    audioContext.resume();
+  }
 
-function stop() {
-    mediaRecorder.stop();
-    stopRecording.disabled = true;
-}
+  // Get access to the user's microphone
+  const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
 
-function startAudioContext() {
-    if (!audioContext) {
-        audioContext = new AudioContext();
-    } else if (audioContext.state === "suspended") {
-        audioContext.resume();
-    }
-}
+  // Create a MediaRecorder object and pass the MediaStream to its constructor
+  mediaRecorder = new MediaRecorder(stream);
 
-startRecording.addEventListener('click', () => {
-    const confirmation = confirm('Do you want to start recording?');
-    if (confirmation) {
-        startAudioContext();
-        start();
-    }
+  // Add an event listener to the MediaRecorder that listens for the "dataavailable" event
+  mediaRecorder.addEventListener('dataavailable', (e) => {
+    recordedChunks.push(e.data);
+  });
+
+  // Start the MediaRecorder
+  mediaRecorder.start();
+
+  // Disable the start recording button and enable the stop recording button
+  startRecording.disabled = true;
+  stopRecording.disabled = false;
 });
 
-stopRecording.addEventListener('click', stop);
+// Add an event listener to the stop recording button
+stopRecording.addEventListener('click', function() {
+  // Stop the MediaRecorder
+  mediaRecorder.stop();
 
-audioInput.addEventListener('change', (e) => {
-    const file = e.target.files[0];
-    // Handle the uploaded file
+  // Disable the stop recording button
+  stopRecording.disabled = true;
+
+  // Prepare the recorded audio data for playback and transcription
+  const blob = new Blob(recordedChunks, { type: 'audio/wav' });
+  const audioURL = URL.createObjectURL(blob);
+
+  // Set the source of the audio element to the recorded audio data
+  const audioElement = document.getElementById('recordedAudio');
+  audioElement.src = audioURL;
+
+  // Transcribe the recorded audio data using the server-side function
+  const formData = new FormData();
+  formData.append('audio_file', blob);
+
+  fetch('/transcribe', {
+    method: 'POST',
+    body: formData
+  })
+  .then(response => response.text())
+  .then(transcription => {
+    // Display the transcribed text in the web page
+    const resultDiv = document.getElementById('result');
+    resultDiv.textContent = transcription;
+  })
+  .catch(error => {
+    console.error(error);
+    alert('Failed to transcribe audio');
+  });
+});
+
+// Add an event listener to the play button
+playButton.addEventListener('click', function() {
+  // Create an AudioContext if one doesn't already exist, or resume the existing one if it's suspended
+  if (!audioContext) {
+    audioContext = new AudioContext();
+  } else if (audioContext.state === 'suspended') {
+    audioContext.resume();
+  }
+
+  // Create an AudioBufferSourceNode and connect it to the AudioContext
+  const source = audioContext.createBufferSource();
+  source.connect(audioContext.destination);
+
+  // Load the audio file using XMLHttpRequest and decode it into an AudioBuffer
+  const request = new XMLHttpRequest();
+  request.open('GET', audioElement.src, true);
+  request.responseType = 'arraybuffer';
+
+  request.onload = function() {
+    audioContext.decodeAudioData(request.response, function(buffer) {
+      // Set the AudioBuffer as the source of the AudioBufferSourceNode
+      source.buffer = buffer;
+      // Start playing the audio
+      source.start(0);
+    });
+  };
+
+  request.send();
 });
