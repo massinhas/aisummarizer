44fae1e6f4401280dd04d80799b2911d8186ced7
Luis Correia <luismassinhas@gmail.com>
2023-03-20 00:50:33 +0000

    update voice thing

--- static/audioHandling.js
+++ static/audioHandling.js
@@ -1,6 +1,8 @@
 const startRecording = document.getElementById('startRecording');
 const stopRecording = document.getElementById('stopRecording');
 const audioInput = document.getElementById('audioInput');
+const audioSummary = document.getElementById('audioSummary');
+const summaryElement = document.getElementById('summary');
 
 let mediaRecorder;
 let recordedChunks = [];
@@ -18,6 +20,51 @@ async function start() {
 function stop() {
     mediaRecorder.stop();
     stopRecording.disabled = true;
+    processRecording();
+}
+
+async function processRecording() {
+    const blob = new Blob(recordedChunks);
+    const formData = new FormData();
+    formData.append('audio', blob);
+
+    try {
+        const response = await fetch('/transcribe', {
+            method: 'POST',
+            body: formData,
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            summaryElement.textContent = 'Summarizing...';
+
+            try {
+                const response = await fetch('/summarize', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ text: data.transcription }),
+                });
+
+                if (response.ok) {
+                    const data = await response.json();
+                    summaryElement.textContent = data.summary;
+                    audioSummary.style.display = 'block';
+                } else {
+                    const errorData = await response.json();
+                    summaryElement.textContent = 'Error: ' + errorData.error;
+                }
+            } catch (error) {
+                summaryElement.textContent = 'Error: ' + error.message;
+            }
+        } else {
+            const errorData = await response.json();
+            summaryElement.textContent = 'Error: ' + errorData.error;
+        }
+    } catch (error) {
+        summaryElement.textContent = 'Error: ' + error.message;
+    }
 }
 
 startRecording.addEventListener('click', start);
